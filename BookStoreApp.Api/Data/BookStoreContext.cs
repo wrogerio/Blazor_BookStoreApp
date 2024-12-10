24b748ffc4b1417a776b1fd84663dcdfb71cda0e
Wellington Rogério <wrogerio.ing@gmail.com>
2024-12-09 22:50:30 -0300

    scaffold database

---
+++ BookStoreApp.Api/Data/BookStoreContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Api.Data;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext()
+    {
+    }
+
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.FirstName).HasMaxLength(60);
+            entity.Property(e => e.LastName).HasMaxLength(60);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.Isbn).HasMaxLength(20);
+            entity.Property(e => e.Summary).HasMaxLength(60);
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BookStoreApp.Api/Program.cs
+++ BookStoreApp.Api/Program.cs
@@ -1,3 +1,5 @@
+using BookStoreApp.Api.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BookStoreContext>(options => options.UseSqlServer(connString));
 
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
