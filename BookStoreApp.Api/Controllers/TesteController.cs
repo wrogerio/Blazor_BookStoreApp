5f0a5914accd50a8f1ba88fc41a6d6b2694f10be
wroge <wroge@WRPC>
2024-12-09 21:45:51 -0300

    Add project files.

---
+++ BookStoreApp.Api/Controllers/TesteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        private readonly ILogger<TesteController> _logger;
+
+        public TesteController(ILogger<TesteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("TesteController.Get() called");
+                
+            try
+            {
+                return Ok("TesteController.Get() called");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
